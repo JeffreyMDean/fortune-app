7fac726080cf78353f5964036db6838c6b2ab9c7
JeffreyMDean <jeffrey.michael.dean@gmail.com>
2024-05-10 18:32:32 -0400

    Troubleshooting routes and controller

--- app/controllers/my_examples_controller.rb
+++ app/controllers/my_examples_controller.rb
@@ -1,8 +1,10 @@
 class MyExamplesController < ApplicationController
+  def get_fortune
+    fortunes = ["You will have a good day", "You will have good health", "You will learn something new"]
+    @fortune = fortunes.sample
+  end
+
+  def lotto
+    @lotto_numbers = (1..60).to_a.sample(6)
+  end
 end
-
-def get_fortune
-  index = 0
-  fortune = rand
-  while index < something.length
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+  get "up" => "rails/health#show", as: :rails_health_check
+  get "fortune", controller: "my_examples", action: "fortune"
+  get "lotto", controller: "my_examples", action: "lotto"
+end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  assert_routing({ path: 'photos', method: :post }, { controller: 'photos', action: 'create' })
+  # assert_routing({ path: 'photos', method: :post }, { controller: 'photos', action: 'create' })
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
-end
+
